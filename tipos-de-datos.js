e55ae307a87b64f112adc20abb6baa1a510f8398
ZAV000 <zoe230205@gmail.com>
2025-02-04 21:37:12 -0600

    Actualización

---
+++ tipos-de-datos.js
@@ -0,0 +1,26 @@
+console.log(typeof 42);          
+//number
+console.log(typeof 'Veinticinco'); 
+//string
+console.log(typeof -666);  
+//number       
+console.log(typeof true);  
+//boolean       
+console.log(typeof 0);    
+//number        
+console.log(typeof '');   
+//string        
+console.log(typeof null);   
+//object      
+console.log(typeof undefined);    
+//undefined
+console.log(typeof FALSE);     
+//undefined 
+console.log(typeof NaN);       
+//number
+console.log(typeof Symbol());  
+//symbol
+console.log(typeof Infinity);  
+//number
+console.log(typeof [1, 2, 3]);
+//object   
